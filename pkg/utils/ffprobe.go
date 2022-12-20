e2d52c515ed1ab79e4441f7f0ac0d447a3d241a7
KonradIT <github@chernowii.com>
2022-12-20 23:38:14 +0100

    Add n-step merging function

--- pkg/utils/ffprobe.go
+++ pkg/utils/ffprobe.go
@@ -3,8 +3,10 @@ package utils
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"os"
 	"os/exec"
+	"strings"
 )
 
 // FFprobe command wrapper
@@ -22,6 +24,13 @@ type VideoSizeResponse struct {
 	} `json:"streams"`
 }
 
+type FramesResponse struct {
+	Programs []interface{} `json:"programs"`
+	Streams  []struct {
+		Frames int `json:"nb_frames,string"`
+	} `json:"streams"`
+}
+
 func NewFFprobe(path *string) FFprobe {
 	ff := FFprobe{}
 	if path == nil {
@@ -32,12 +41,12 @@ func NewFFprobe(path *string) FFprobe {
 	return ff
 }
 
-func (f *FFprobe) VideoSize(path string) (*VideoSizeResponse, error) {
+func (f *FFprobe) executeGetInfo(path string, entries ...string) ([]byte, error) {
 	args := []string{
 		"-select_streams",
 		"v:0",
 		"-show_entries",
-		"stream=width,height,r_frame_rate",
+		fmt.Sprintf("stream=%s", strings.Join(entries, ",")),
 		"-of",
 		"json",
 		path,
@@ -53,9 +62,29 @@ func (f *FFprobe) VideoSize(path string) (*VideoSizeResponse, error) {
 	if err != nil {
 		return nil, err
 	}
+	return out.Bytes(), nil
+}
 
+func (f *FFprobe) VideoSize(path string) (*VideoSizeResponse, error) {
 	result := VideoSizeResponse{}
-	err = json.Unmarshal(out.Bytes(), &result)
+	out, err := f.executeGetInfo(path, "width", "height", "r_frame_rate")
+	if err != nil {
+		return nil, err
+	}
+	err = json.Unmarshal(out, &result)
+	if err != nil {
+		return nil, err
+	}
+	return &result, nil
+}
+
+func (f *FFprobe) Frames(path string) (*FramesResponse, error) {
+	result := FramesResponse{}
+	out, err := f.executeGetInfo(path, "nb_frames")
+	if err != nil {
+		return nil, err
+	}
+	err = json.Unmarshal(out, &result)
 	if err != nil {
 		return nil, err
 	}
--- pkg/videomanipulation/ffmpeg.go
+++ pkg/videomanipulation/ffmpeg.go
@@ -1,10 +1,18 @@
 package videomanipulation
 
 import (
+	"fmt"
 	"io/ioutil"
+	"log"
+	"path/filepath"
+	"strconv"
 	"strings"
 	"sync"
+	"time"
 
+	"github.com/konradit/mmt/pkg/utils"
+	"github.com/vbauerster/mpb/v8"
+	"github.com/vbauerster/mpb/v8/decor"
 	"github.com/xfrr/goffmpeg/ffmpeg"
 	"github.com/xfrr/goffmpeg/transcoder"
 )
@@ -17,6 +25,139 @@ func New() {
 	conf.FfprobeBin = strings.Trim(conf.FfprobeBin, "\r")
 	conf.FfmpegBin = strings.Trim(conf.FfmpegBin, "\r")
 	trans.SetConfiguration(conf)
+}
+
+func getBar(progressBar *mpb.Progress, total int, name string) *mpb.Bar {
+	return progressBar.AddBar(int64(total),
+		mpb.PrependDecorators(
+			decor.Name(name),
+			decor.Percentage(decor.WCSyncSpace),
+		),
+		mpb.AppendDecorators(
+			decor.OnComplete(
+				decor.EwmaETA(decor.ET_STYLE_GO, 60, decor.WCSyncWidth), "✔️",
+			),
+		),
+	)
+}
+
+//nolint:golint,unused,errcheck
+func merge(bar *mpb.Bar, videos ...string) error {
+	err := trans.InitializeEmptyTranscoder()
+	if err != nil {
+		return err
+	}
+
+	trans.MediaFile().SetRawInputArgs([]string{"-hwaccel", "cuda"})
+	intermediates := []string{}
+
+	for _, x := range videos {
+		intermediates = append(intermediates, strings.Replace(x, ".MP4", ".ts", -1))
+	}
+	err = trans.SetInputPath(fmt.Sprintf("concat:%s",
+		strings.Join(intermediates, "|"),
+	),
+	)
+	if err != nil {
+		return err
+	}
+
+	err = trans.SetOutputPath(
+		strings.Replace(videos[0], "-01.MP4", "-merged.MP4", -1))
+	if err != nil {
+		return err
+	}
+	trans.MediaFile().SetVideoCodec("copy")
+	trans.MediaFile().SetAudioCodec("copy")
+
+	done := trans.Run(true)
+
+	progress := trans.Output()
+
+	for msg := range progress {
+		s, _ := strconv.Atoi(msg.FramesProcessed)
+		bar.SetCurrent(int64(s))
+	}
+
+	err = <-done
+	return err
+}
+
+//nolint:golint,unused,errcheck
+func convert(video string, bar *mpb.Bar) error {
+	err := trans.InitializeEmptyTranscoder()
+	if err != nil {
+		return err
+	}
+
+	trans.MediaFile().SetRawInputArgs([]string{"-hwaccel", "cuda"})
+	err = trans.SetInputPath(video)
+	if err != nil {
+		return err
+	}
+
+	err = trans.SetOutputPath(
+		strings.Replace(video, ".MP4", ".ts", -1))
+	if err != nil {
+		return err
+	}
+	trans.MediaFile().SetVideoCodec("copy")
+	trans.MediaFile().SetAudioCodec("copy")
+	trans.MediaFile().SetRawOutputArgs([]string{"-bsf:v", "h264_mp4toannexb"})
+	trans.MediaFile().SetOutputFormat("mpegts")
+
+	done := trans.Run(true)
+
+	progress := trans.Output()
+
+	for msg := range progress {
+		s, _ := strconv.Atoi(msg.FramesProcessed)
+		bar.SetCurrent(int64(s))
+	}
+
+	err = <-done
+	return err
+}
+
+func Merge(videos ...string) error {
+	var wg sync.WaitGroup
+	p := mpb.New(mpb.WithWaitGroup(&wg),
+		mpb.WithWidth(60),
+		mpb.WithRefreshRate(180*time.Millisecond))
+	nfiles := len(videos)
+
+	totalFrames := 0
+	for i := 0; i < nfiles; i++ {
+		ffprobe := utils.NewFFprobe(nil)
+
+		head, err := ffprobe.Frames(videos[i])
+		if err != nil {
+			return err
+		}
+		totalFrames += head.Streams[0].Frames
+		bar := getBar(p, head.Streams[0].Frames, filepath.Base(videos[i]))
+
+		wg.Add(1)
+		go func(current int) {
+			defer wg.Done()
+			err := convert(videos[current], bar)
+			if err != nil {
+				log.Fatal(err.Error())
+			}
+		}(i)
+	}
+	p.Wait()
+
+	nonAsync := mpb.New(
+		mpb.WithWidth(60),
+		mpb.WithRefreshRate(180*time.Millisecond))
+	newBar := getBar(nonAsync, totalFrames, "Merging")
+	err := merge(newBar, videos...)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	nonAsync.Wait()
+	return nil
 }
 
 //nolint:golint,unused,errcheck
