1b371752c80e3c5ba8a4c75a0ec830f3f3eaede5
KonradIT <github@chernowii.com>
2022-12-20 16:21:47 +0100

    WIP for Insta360 X3 usb mode

--- pkg/insta360/update.go
+++ pkg/insta360/update.go
@@ -5,14 +5,14 @@ import (
 	"fmt"
 	"net/http"
 	"path/filepath"
-	"strings"
+	"time"
 
 	"github.com/fatih/color"
 	"github.com/k3a/html2text"
 	"github.com/konradit/mmt/pkg/utils"
 )
 
-var FirmwareCatalogRemoteURL = "https://openapi.insta360.com/website/appDownload/getGroupApp?group=%s&X-Language=en-us"
+var FirmwareCatalogRemoteURL = "https://service.insta360.com/app-service/app/appDownload/getGroupApp?group=%s&X-Language=en-us"
 
 func UpdateCamera(sdcard string, model string) error {
 	client := &http.Client{}
@@ -37,22 +37,26 @@ func UpdateCamera(sdcard string, model string) error {
 	}
 
 	for _, firmware := range response.Data.Apps {
-		for _, item := range firmware.Items {
-			if item.Platform == "insta360" {
-				color.Cyan("☁️ Firmware: [%s]", item.Version)
-				color.Yellow(">> Firmware release date: %s", item.UpdateTime)
-				color.White(html2text.HTML2Text(item.Description))
-
-				fwURL := item.Channels[0].DownloadURL
-				err = utils.DownloadFile(filepath.Join(sdcard, strings.Split(fwURL, "/")[len(strings.Split(fwURL, "/"))-1]), fwURL)
-				if err != nil {
-					return err
-				}
-				color.Cyan("Firmware downloaded to SD card!")
-				color.Cyan("Now eject the SD card and insert it into your camera")
-				color.Cyan("then turn your camera on and wait for it to update")
-			}
+		if firmware.Name != "Camera firmware" {
+			continue
 		}
+		item := firmware.Items[len(firmware.Items)-1]
+		color.Cyan("Insta360 Firmware:\n☁️ Version: [%s]", item.Version)
+		updateTime := time.UnixMilli(item.UpdateTime)
+
+		color.Yellow("📅 Release date: [%s]", updateTime.Format(time.RFC822))
+		color.White(html2text.HTML2Text(item.Description))
+
+		fwURL := item.Channels[0].DownloadURL
+
+		err = utils.DownloadFile(filepath.Join(sdcard, filepath.Base(fwURL)), fwURL)
+		if err != nil {
+			return err
+		}
+		color.Cyan("Firmware downloaded to SD card!")
+		color.Cyan("Now eject the SD card and insert it into your camera")
+		color.Cyan("then turn your camera on and wait for it to update")
+		break
 	}
 	return nil
 }
