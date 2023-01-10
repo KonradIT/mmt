e57863c738063f658d44e4775e613a5275f80289
Daktak <daktak@domo.ddns.net>
2023-01-10 16:28:45 +1100

    update

--- cmd/info.go
+++ cmd/info.go
@@ -1,6 +1,8 @@
 package cmd
 
 import (
+	"fmt"
+
 	"github.com/erdaltsksn/cui"
 	"github.com/konradit/mmt/pkg/gopro"
 	"github.com/konradit/mmt/pkg/utils"
@@ -29,10 +31,63 @@ var infoCmd = &cobra.Command{
 			}
 			switch c {
 			case utils.GoPro:
-				err = gopro.GetInfo(input)
+				var gpStatus = gopro.CameraStatus{}
+				gpStatus, err = gopro.GetInfo(input)
+				fmt.Printf("SSID : %s\n", gpStatus.Status.WiFiSSID)
+				currentMode := gpStatus.Status.CurrentMode
+				var modeName = "Video"
+				var whiteBal = gpStatus.Settings.Num11
+				var isoMode = gpStatus.Settings.Num74
+				var isoLimit = gpStatus.Settings.Num13
+				var isoMin = 0
+				var proTune = gpStatus.Settings.Num10
+				switch currentMode {
+				case 1:
+					modeName = "Photo"
+					whiteBal = gpStatus.Settings.Num22
+					isoMin = gpStatus.Settings.Num75
+					isoLimit = gpStatus.Settings.Num24
+					proTune = gpStatus.Settings.Num21
+				case 2:
+					modeName = "MultiShot"
+					whiteBal = gpStatus.Settings.Num35
+					isoMin = gpStatus.Settings.Num76
+					isoLimit = gpStatus.Settings.Num37
+					proTune = gpStatus.Settings.Num34
+				}
+				fmt.Printf("Mode : %s\n", modeName)
+				fmt.Printf("White Balance : %t\n", whiteBal != 0)
+				if currentMode == 0 {
+					var isoText = ""
+					if isoMode == 1 {
+						isoText = "Lock"
+					} else {
+						isoText = "Max"
+					}
+					fmt.Printf("ISO Mode : %s\n", isoText)
+				} else {
+					fmt.Printf("ISO Min : %d\n", getISO(isoMin))
+				}
+				fmt.Printf("ISO Limit : %d\n", getISO(isoLimit))
+				fmt.Printf("Protune : %t\n", proTune != 0)
 			}
 		}
 	},
+}
+
+func getISO(in int) int {
+	switch in {
+	case 0:
+		return 800
+	case 1:
+		return 400
+	case 2:
+		return 200
+	case 3:
+		return 100
+	default:
+		return 0
+	}
 }
 
 func init() {
--- pkg/gopro/info.go
+++ pkg/gopro/info.go
@@ -3,7 +3,6 @@ package gopro
 /* GoPro Connect - API exposed over USB Ethernet */
 
 import (
-        "fmt"
         /*
 	mErrors "github.com/konradit/mmt/pkg/errors"
 	"github.com/konradit/mmt/pkg/utils"
@@ -11,14 +10,12 @@ import (
         */
 )
 
-func GetInfo(in string) error {
+func GetInfo(in string) (CameraStatus, error) {
 	var gpStatus = &CameraStatus{}
 	err := caller(in, "gp/gpControl/status", gpStatus)
 	if err != nil {
-               fmt.Println(err)
-               return err
+               return *gpStatus, err
 	}
-        fmt.Println(gpStatus)
-        return nil
+        return *gpStatus, nil
 }
 
