a7217e101dbd3474110f755af56eafc94e785e62
Konrad Iturbe <KonradIT@users.noreply.github.com>
2023-02-07 14:53:06 +0100

    Cleanup name and test for different coordinates (#100)

---
+++ pkg/utils/reversegeo_test.go
@@ -0,0 +1,47 @@
+package utils
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/codingsince1985/geo-golang/openstreetmap"
+	"github.com/stretchr/testify/require"
+)
+
+type pair struct {
+	Address Location
+	Result  []string
+}
+
+func TestPrettyAddress(t *testing.T) {
+	expected := []pair{
+		{
+			Address: Location{
+				Latitude:  -39.6375091,
+				Longitude: 175.2222849,
+			},
+			Result: []string{"Manawatū-Whanganui New Zealand _ Aotearoa", "New Zealand _ Aotearoa"},
+		},
+		{
+			Address: Location{
+				Latitude:  52.547567,
+				Longitude: 13.385176,
+			},
+			Result: []string{"Berlin Deutschland", "Deutschland"},
+		},
+	}
+
+	for valueIndex, value := range expected {
+		t.Run(fmt.Sprintf("Pair %d", valueIndex), func(t *testing.T) {
+			service := openstreetmap.Geocoder()
+			address, err := service.ReverseGeocode(value.Address.Latitude, value.Address.Longitude)
+			require.NoError(t, err)
+
+			resp := getPrettyAddress(format1{}, address)
+			require.Equal(t, value.Result[0], resp)
+
+			resp = getPrettyAddress(format2{}, address)
+			require.Equal(t, value.Result[1], resp)
+		})
+	}
+}
