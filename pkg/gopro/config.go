59bd77764c4c49e8cbcb007ed6ad1b8363e70f4d
Fco Javier Felix <ffelix@inode64.com>
2023-04-11 22:00:53 +0200

    Improves the process of finding the correct gps location of the video. I have reviewed all my gopro videos and made all improvements to improve accuracy I have removed the GpsFix and because some videos recorded with the gopro have the correct location but the gpsfix flag was not active, the same thing also happened with the speed that there were videos with the correct location but at speed 0
    Then I have added a process to select the most repeated and next locations so we discard false locations.
    we also control the height because it is an indicator of incorrect position.
    
    That's why in the end I added a filter by country, so that they discard the false locations, but this generates an infinite loop that makes the videos repeat over and over again.
    I have gone crazy and I have not been able to solve it
    
    Please help
    
    All the best

--- pkg/gopro/config.go
+++ pkg/gopro/config.go
@@ -8,14 +8,20 @@ import (
 
 const parent = "gopro"
 
-func gpsLockTypesFromConfig() []int {
-	key := fmt.Sprintf("%s.gps_fix", parent)
-	viper.SetDefault(key, []int{2, 3}) // 3d lock, 2d lock
-	return viper.GetIntSlice(key)
-}
-
 func gpsMinAccuracyFromConfig() uint16 {
 	key := fmt.Sprintf("%s.gps_accuracy", parent)
 	viper.SetDefault(key, 500)
 	return uint16(viper.GetUint(key))
+}
+
+func gpsMaxAltitudeFromConfig() float64 {
+	key := fmt.Sprintf("%s.gps_max_altitude", parent)
+	viper.SetDefault(key, 8000)
+	return float64(viper.GetUint(key))
+}
+
+func gpsCountryCodesFromConfig() []string {
+	key := fmt.Sprintf("%s.gps_country_codes", parent)
+	viper.SetDefault(key, []string{}) // 3d lock, 2d lock
+	return viper.GetStringSlice(key)
 }
--- pkg/gopro/location.go
+++ pkg/gopro/location.go
@@ -5,15 +5,22 @@ import (
 	"io"
 	"path/filepath"
 	"strings"
+	"math"
 
 	"github.com/konradit/gopro-utils/telemetry"
 	mErrors "github.com/konradit/mmt/pkg/errors"
 	"github.com/konradit/mmt/pkg/utils"
 	"github.com/konradit/mmt/pkg/videomanipulation"
+	"github.com/codingsince1985/geo-golang/openstreetmap"
 	"golang.org/x/exp/slices"
 )
 
 type LocationService struct{}
+
+type Location struct {
+	latitude float64
+	longitude float64
+}
 
 func (LocationService) GetLocation(path string) (*utils.Location, error) {
 	switch true {
@@ -37,9 +44,11 @@ func fromMP4(videoPath string) (*utils.Location, error) {
 		return nil, err
 	}
 
+	GPSNum := 0
 	reader := bytes.NewReader(*data)
 
 	lastEvent := &telemetry.TELEM{}
+	coordinates := []Location{}
 
 	for {
 		event, err := telemetry.Read(reader)
@@ -62,16 +71,90 @@ func fromMP4(videoPath string) (*utils.Location, error) {
 
 		telems := lastEvent.ShitJson()
 		for _, telem := range telems {
-			if telem.Speed == 0 || telem.Latitude == 0 || telem.Longitude == 0 || telem.GpsAccuracy > gpsMinAccuracyFromConfig() || !slices.Contains(gpsLockTypesFromConfig(), int(telem.GpsFix)) {
+			if telem.Altitude > gpsMaxAltitudeFromConfig() || telem.Latitude == 0 || telem.Longitude == 0 || telem.GpsAccuracy > gpsMinAccuracyFromConfig() {
 				continue
 			}
-			return &utils.Location{
-				Latitude:  telem.Latitude,
-				Longitude: telem.Longitude,
-			}, nil
+
+			CountryCodes := gpsCountryCodesFromConfig()
+			if len(CountryCodes) != 0 {
+				service := openstreetmap.Geocoder()
+
+            	address, err := service.ReverseGeocode(telem.Latitude, telem.Longitude)
+            	if err != nil {
+            		continue
+            	}
+            	if slices.Contains(CountryCodes, address.CountryCode) {
+            		coordinates = append(coordinates, Location{telem.Latitude, telem.Longitude})
+
+            		GPSNum++
+            		// TODO: get from config
+            		if GPSNum > 3 {
+						return &utils.Location{
+							Latitude:  telem.Latitude,
+							Longitude: telem.Longitude,
+						}, nil
+            		}
+				}
+			}
+
+			coordinates = append(coordinates, Location{telem.Latitude, telem.Longitude})
 		}
 		*lastEvent = *event
 	}
 
-	return nil, mErrors.ErrNoGPS
+    if len(coordinates) == 0 {
+		return nil, mErrors.ErrNoGPS
+	}
+
+	closestLocation := getClosestLocation(coordinates)
+
+	return &utils.Location{
+		Latitude:  closestLocation.latitude,
+		Longitude: closestLocation.longitude,
+	}, nil
+}
+
+func getClosestLocation(locations []Location) Location {
+	// Find the nearest and repeat location
+	counts := make(map[Location]int)
+	for _, loc := range locations {
+		counts[loc]++
+	}
+
+	mostFrequentLocation := Location{}
+	maxCount := 0
+	for loc, count := range counts {
+		if count > maxCount {
+			mostFrequentLocation = loc
+			maxCount = count
+		} else if count == maxCount {
+			// If there are multiple locations with the same frequency, choose the closest one
+			distanceToLoc := distance(locations[0], loc)
+			distanceToMostFrequent := distance(locations[0], mostFrequentLocation)
+			if distanceToLoc < distanceToMostFrequent {
+				mostFrequentLocation = loc
+			}
+		}
+	}
+
+	return mostFrequentLocation
+}
+
+func distance(loc1 Location, loc2 Location) float64 {
+	lat1 := degreesToRadians(loc1.latitude)
+	lon1 := degreesToRadians(loc1.longitude)
+	lat2 := degreesToRadians(loc2.latitude)
+	lon2 := degreesToRadians(loc2.longitude)
+
+	deltaLat := lat2 - lat1
+	deltaLon := lon2 - lon1
+
+	a := math.Pow(math.Sin(deltaLat/2), 2) + math.Cos(lat1)*math.Cos(lat2)*math.Pow(math.Sin(deltaLon/2), 2)
+	c := 2 * math.Atan2(math.Sqrt(a), math.Sqrt(1-a))
+
+	return 6371 * c // radius of the earth in km
+}
+
+func degreesToRadians(degrees float64) float64 {
+	return degrees * math.Pi / 180
 }
