d9af87028e83ecd62e56d8f817f8016204c9edc6
Francisco Javier <ffelix@inode64.com>
2023-04-27 20:04:35 +0200

    Fix regex expression (#106)

--- pkg/gopro/filetypes.go
+++ pkg/gopro/filetypes.go
@@ -5,95 +5,95 @@ import "regexp"
 var FileTypeMatches = map[Type][]FileTypeMatch{
 	V2: {
 		{
-			Regex:    regexp.MustCompile(`GOPR\d+.JPG`),
+			Regex:    regexp.MustCompile(`^GOPR\d+\.JPG$`),
 			Type:     Photo,
 			HeroMode: true,
 		},
 		{
-			Regex:    regexp.MustCompile(`GP\d+.JPG`),
+			Regex:    regexp.MustCompile(`^GP\d+\.JPG$`),
 			Type:     Photo,
 			HeroMode: true,
 		},
 		{
-			Regex:    regexp.MustCompile(`GX\d+.MP4`),
+			Regex:    regexp.MustCompile(`^GX\d+\.MP4$`),
 			Type:     Video,
 			HeroMode: true,
 		},
 		{
-			Regex:    regexp.MustCompile(`GX\d+.WAV`),
+			Regex:    regexp.MustCompile(`^GX\d+\.WAV$`),
 			Type:     Audio,
 			HeroMode: true,
 		},
 		{
-			Regex:    regexp.MustCompile(`GH\d+.MP4`),
+			Regex:    regexp.MustCompile(`^GH\d+\.MP4$`),
 			Type:     Video,
 			HeroMode: true,
 		},
 		{
-			Regex:    regexp.MustCompile(`GG\d+.MP4`), // Live Bursts...
+			Regex:    regexp.MustCompile(`^GG\d+\.MP4$`), // Live Bursts...
 			Type:     Video,
 			HeroMode: true,
 		},
 		{
-			Regex:    regexp.MustCompile(`G\d+.JPG`),
+			Regex:    regexp.MustCompile(`^G\d+\.JPG$`),
 			Type:     Multishot,
 			HeroMode: true,
 		},
 		{
-			Regex:    regexp.MustCompile(`.GPR`),
+			Regex:    regexp.MustCompile(`\.GPR$`),
 			Type:     RawPhoto,
 			HeroMode: true,
 		},
 		// 360 formats, just MAX for now
 		{
-			Regex:    regexp.MustCompile(`GS\d+.360`),
+			Regex:    regexp.MustCompile(`^GS\d+\.360$`),
 			Type:     Video,
 			HeroMode: false,
 		},
 		{
-			Regex:    regexp.MustCompile(`GS_+\d+.JPG`),
+			Regex:    regexp.MustCompile(`^GS_+\d+\.JPG$`),
 			Type:     Photo,
 			HeroMode: false,
 		},
 		{
-			Regex:    regexp.MustCompile(`GP_+\d+.JPG`),
+			Regex:    regexp.MustCompile(`^GP_+\d+\.JPG$`),
 			Type:     Photo,
 			HeroMode: true,
 		},
 		{
-			Regex:    regexp.MustCompile(`GPA[A-Z]\d+.JPG`),
+			Regex:    regexp.MustCompile(`^GPA[A-Z]\d+\.JPG$`),
 			Type:     Multishot,
 			HeroMode: true,
 		},
 		{
-			Regex:    regexp.MustCompile(`GSA[A-Z]\d+.JPG`),
+			Regex:    regexp.MustCompile(`^GSA[A-Z]\d+\.JPG$`),
 			Type:     Multishot,
 			HeroMode: false,
 		},
 	},
 	V1: {
 		{
-			Regex:    regexp.MustCompile(`GOPR\d+.JPG`),
+			Regex:    regexp.MustCompile(`^GOPR\d+\.JPG$`),
 			Type:     Photo,
 			HeroMode: true,
 		},
 		{
-			Regex:    regexp.MustCompile(`G\d+.JPG`),
+			Regex:    regexp.MustCompile(`^G\d+\.JPG$`),
 			Type:     Multishot,
 			HeroMode: true,
 		},
 		{
-			Regex:    regexp.MustCompile(`GOPR\d+.MP4`),
+			Regex:    regexp.MustCompile(`^GOPR\d+\.MP4$`),
 			Type:     Video,
 			HeroMode: true,
 		},
 		{
-			Regex:    regexp.MustCompile(`GP\d+.MP4`),
+			Regex:    regexp.MustCompile(`^GP\d+\.MP4$`),
 			Type:     ChapteredVideo,
 			HeroMode: true,
 		},
 		{
-			Regex:    regexp.MustCompile(`.GPR`),
+			Regex:    regexp.MustCompile(`\.GPR$`),
 			Type:     RawPhoto,
 			HeroMode: true,
 		},
